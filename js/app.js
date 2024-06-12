c726505e8d8c2dc61127ab2b523e06807c55038e
nahidulDTH <nahidhk2007@gmail.com>
2024-06-12 23:26:54 +0600

    update

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function cngnam1(){
+  document.getElementById("cid").innerHTML="Ataikula Madpur Amena Khutun Digger Celloge"
+  setTimeout(cngnam2,1000);
+}
+
+function cngnam2(){
+  document.getElementById("cid").innerHTML="আতাইকুলা মাধপুর আমেনা খাতুন ডিগ্রী কলেজ।"
+  setTimeout(cngnam1,1000);
+
+}
+
+setTimeout(cngnam1,1000);
